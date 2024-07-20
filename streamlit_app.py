b08aa15cbfae2f8e263028e56d745d41ce0568e4
houred <Houred2030@gmail.com>
2024-07-20 22:18:43 +0000

    2222

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title(" My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
 st.write(
-    "Salam aalaykoum"
+    "Salam aalaykoum 🎈"
 )
